89c966ceae2bb3c7df4eabc6007ca219660e3240
Ram Chandra Limbu <ramchandralimbu75@gmail.com>
2023-06-21 13:06:03 +0545

    error aayo

---
+++ src/components/OurPartners/OurPartners.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import OurPartners from '../OurPartners/OurPartners';
+
+import MainContainer from "../AboutUs/MainContainer/MainContainer";
+
+const OurPartners = () => {
+  return(
+    <>
+        <NavBar/>
+        
+        
+    </>
+  )
+}
+
+export default OurPartners;
